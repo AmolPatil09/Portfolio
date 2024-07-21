2db76d523ac9bca425a3ce6a8fadf0ecf4d25d70
Amol Patil <amol17092000@gmail.com>
2024-07-21 16:01:08 +0530

    changes in project comp

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -5,10 +5,11 @@ import hospital from '../assets/img1.jpeg'
 export default function Projects() {
   return (
     <div>
-    <div>
-     <h1 className='mt-5 text-2xl'>Projects</h1>
-     <div className='flex flex-wrap sm:flex-row flex-col justify-center'>
-          <div className='border-2 min-h-48 sm:w-96 min-w-56 w-40 border-amber-100  mx-auto  m-6 sm:mx-[50px] rounded-md'>
+      <h1 className='mt-5 text-2xl'>Projects</h1>
+    <div className='sm:flex '>
+     
+     <div className='flex flex-wrap  justify-center m-auto'>
+          <div className='border-2 min-h-48 sm:w-96 min-w-56 w-56 border-amber-100  mx-auto  m-6 sm:mx-[50px] rounded-md'>
           <h1 className='font-bold my-2'>eshop</h1>
           <img src={img} className=' m-[5%] h-80 w-[90%]' />
           <div className='flex justify-center'>
@@ -16,11 +17,7 @@ export default function Projects() {
           <h1 className='font-bold m-5'><a href='https://github.com/AmolPatil09/eShop' target='_blank'>Code</a></h1>
           </div>
           </div>
-     </div>
-
-    </div>
-      <div>
-      <div className='flex flex-wrap sm:flex-row flex-col justify-center'>
+          <div className='flex flex-wrap sm:flex-row flex-col justify-center m-auto'>
            <div className='border-2 min-h-48 sm:w-96 min-w-56 w-40 border-amber-100  mx-auto  m-6 sm:mx-[50px] rounded-md'>
            <h1 className='font-bold my-2'>CareHealth</h1>
            <img src={hospital} className=' m-[5%] h-80 w-[90%]' />
@@ -30,6 +27,11 @@ export default function Projects() {
            </div>
            </div>
       </div>
+     </div>
+
+    </div>
+      <div>
+    
  
      </div>
      </div>
